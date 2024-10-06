a197b5e9d0109d398bf6d3f0ecdfb80357ecb21a
suren-11 <presanth11@gmail.com>
2024-10-06 19:10:06 +0530

    create GreetingController and first get Method

---
+++ Controllers/GreetingController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationTraining.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetingController : ControllerBase
+    {
+        [HttpGet("greet")]
+        public IActionResult Greet([FromQuery]string fullName, [FromQuery]DateTime dateOfBirth)
+        {
+            int age = DateTime.Now.Year - dateOfBirth.Year;
+
+            return Ok($"Hello, my name is {fullName} and I am {age} years old..");
+        }
+    }
+}
