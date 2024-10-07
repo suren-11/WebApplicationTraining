1e3337a9a564da3ff3bb48c3792a1e7174dcccda
suren-11 <presanth11@gmail.com>
2024-10-07 09:04:15 +0530

    defiend new get method for name

--- Controllers/GreetingController.cs
+++ Controllers/GreetingController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections;
 
 namespace WebApplicationTraining.Controllers
 {
@@ -7,12 +8,34 @@ namespace WebApplicationTraining.Controllers
     [ApiController]
     public class GreetingController : ControllerBase
     {
-        [HttpGet("greet")]
-        public IActionResult Greet([FromQuery]string fullName, [FromQuery]DateTime dateOfBirth)
+        [HttpGet]
+        public IActionResult Greet(string fullName, DateTime dateOfBirth)
         {
             int age = DateTime.Now.Year - dateOfBirth.Year;
 
             return Ok($"Hello, my name is {fullName} and I am {age} years old..");
+        }
+
+        [HttpGet("findname")]
+        public IActionResult getName(string name)
+        {
+            ArrayList fullNames = new ArrayList() { "Suren Prasanth", "Alex Pandian", "Prasanga Basnayake", "Ramesh Gunathilaka", "Alex Perera", "Suren Ram", "Prasanga Rathnayake" };
+            ArrayList matchingNames = new ArrayList();
+
+            foreach (string fullName in fullNames)
+            {
+                if (fullName.IndexOf(name, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    matchingNames.Add(fullName);
+                }
+            }
+
+            if (matchingNames.Count > 0)
+            {
+                return Ok(matchingNames);
+            }
+
+            return NotFound("No matching names found");
         }
     }
 }
